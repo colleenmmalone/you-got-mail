7d735ea04a9ad7b9aa0fd6e1d7ec0770ca27ede4
colleenmmalone <colleen.malone@protonmail.com>
2026-03-06 16:35:56 -0500

    maybe fixed preview image

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import SentLetter from "./pages/SentLetter";
+import CoverImage from "./pages/CoverImage";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/letter" element={<SentLetter />} />
+          <Route path="/cover" element={<CoverImage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/CoverImage.tsx
@@ -0,0 +1,8 @@
+import cover from '../assets/cover-image.png'
+
+const CoverImage = () => {
+
+  return (<img src={cover} alt={"cover image"} />)
+};
+
+export default CoverImage;
